602ed65be808a01920653bdb380387136224360e
vlobos <suryvlobos@gmail.com>
2019-01-02 15:04:00 -0800

    install cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 
 const server = express();
 const port = 3000;
@@ -12,7 +13,9 @@ server.use(bodyParser.urlencoded({extended:true}));
 
 server.use(express.static(path.join(__dirname, "../public")));
 
-server.use("/",router)
+server.use(cors());
+
+server.use("/",router);
 
 server.listen(3000, ()=>{
   console.log("Listening to port: ", port)
