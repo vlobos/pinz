2066f7c6cb676d2f0dd225a42d127faac73e4036
vlobos <suryvlobos@gmail.com>
2019-01-02 15:40:52 -0800

    implement controller and model to handle post request

--- db/controllers.js
+++ db/controllers.js
@@ -2,7 +2,17 @@ const scoreModel =  require("./models");
 
 module.exports = {
   get: (req,res)=> {
-    res.send("Controllers");
+    res.send("Get Controllers");
     scoreModel.get();
+  },
+  post: (req,res)=> {
+    console.log("Request: ",req.body.game);
+    scoreModel.post(req.body.game, (err)=>{
+      if(err){
+        throw err;
+      }else{
+        res.send("Post Controller");
+      }
+    });
   }
 }
--- db/models.js
+++ db/models.js
@@ -2,7 +2,14 @@ const database = require("./config");
 
 module.exports = {
   get: ()=> {
-    console.log("Models Fired")
-   
+    console.log("Get Models Fired")
+  },
+  post: (data,callback) =>{
+    console.log("Post Models Fired")
+    let query = `INSERT INTO games (player, frame_one, frame_two, frame_three, frame_four, frame_five, frame_six, frame_seven, frame_eight,frame_nine, frame_ten, score) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`
+    database.query(query, data, (err)=>{
+      if(err) throw err;
+      callback(err)
+    })
   }
 }
