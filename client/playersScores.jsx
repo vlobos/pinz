b0a12f8fa04e9738c548b9473ac1591eeec3f692
vlobos <suryvlobos@gmail.com>
2019-01-02 20:10:53 -0800

    add classes and ids to html for styling

--- client/playersScores.jsx
+++ client/playersScores.jsx
@@ -9,17 +9,19 @@ class PlayersScores extends React.Component{
     console.log("PROPS : ",this.props)
     return(
       <React.Fragment>
-        <div>HIGH SCORES</div>
-        <ul>
-          {this.props.pastScores.map((element, index)=>{
-            return(
-              <li>
-                <div>{element.player}</div>
-                <div>{element.score}</div>
-              </li>
-            )
-          })}
-        </ul>
+        <div id="high__scores">
+          <div id="high__scores__title">HIGH SCORES</div>
+          <ul className="high__scores__list">
+            {this.props.pastScores.map((element, index)=>{
+              return(
+                <li>
+                  <div>{element.player}</div>
+                  <div>{element.score}</div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </React.Fragment>
     )
   }
