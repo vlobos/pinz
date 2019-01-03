2507618bb432be7623436b71026e0999b25e0dc0
vlobos <suryvlobos@gmail.com>
2019-01-02 17:00:45 -0800

    change score from string to number

--- db/models.js
+++ db/models.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   post: (data,callback) =>{
     console.log("Post Models Fired")
-    let query = `INSERT INTO games (player, frame_one, frame_two, frame_three, frame_four, frame_five, frame_six, frame_seven, frame_eight,frame_nine, frame_ten, score) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`
+    let totalScore = Number(data[12]);
+    let query = `INSERT INTO games (player, frame_one, frame_two, frame_three, frame_four, frame_five, frame_six, frame_seven, frame_eight,frame_nine, frame_ten, score) VALUES (?,?,?,?,?,?,?,?,?,?,?,${totalScore})`
     database.query(query, data, (err)=>{
       if(err) throw err;
       callback(err)
