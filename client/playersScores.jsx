cd9a98f4cabc37fbb4df1c52a1ba69424589b0a2
vlobos <suryvlobos@gmail.com>
2019-01-03 13:56:21 -0800

    modify html to style scrollbar

--- client/playersScores.jsx
+++ client/playersScores.jsx
@@ -10,16 +10,18 @@ class PlayersScores extends React.Component{
       <React.Fragment>
         <div id="high__scores">
           <div id="high__scores__title">HIGH SCORES</div>
-          <ul className="high__scores__list">
-            {this.props.pastScores.map((element, index)=>{
-              return(
-                <li className="high__scores__li">
-                  <div className="high__scores__div">{element.player}</div>
-                  <div className="high__scores__div">{element.score}</div>
-                </li>
-              )
-            })}
-          </ul>
+          <div className="high__scores__list">
+            <ul>
+              {this.props.pastScores.map((element, index)=>{
+                return(
+                  <li className="high__scores__li">
+                    <div className="high__scores__div">{element.player}</div>
+                    <div className="high__scores__div">{element.score}</div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </React.Fragment>
     )
